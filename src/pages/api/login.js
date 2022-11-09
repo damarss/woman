5511aec48cd886fea6d39737ae1c63495b0354d9
Damars <222011829@stis.ac.id>
2022-11-09 08:40:30 +0700

    Expires token 1 hari

--- src/pages/api/login.js
+++ src/pages/api/login.js
@@ -24,7 +24,7 @@ export default async function handle(req, res) {
             return res.status(400).json({ message: 'Invalid email or password' });
         }
 
-        let token = jwt.sign({uid: user.id, name: user.name, iat: Math.floor(Date.now() / 1000) - 30, exp: Math.floor(Date.now() / 1000) + (60 * 60)}, process.env.JWT_SECRET, {algorithm: 'HS256'});
+        let token = jwt.sign({uid: user.id, name: user.name, iat: Math.floor(Date.now() / 1000) - 30, exp: Math.floor(Date.now() / 1000) + (60 * 60 * 24)}, process.env.JWT_SECRET, {algorithm: 'HS256'});
 
         return res.status(200).json({ message: 'Logged in successfully', token: token });
     }
