b97d596a85d2b3e960796f05760ffd94d03c2523
arfiandys <222011791@stis.ac.id>
2022-11-11 07:19:58 +0700

    merubah isi schedular

--- src/pages/meeting/index.js
+++ src/pages/meeting/index.js
@@ -2,12 +2,16 @@
 import Grid from '@mui/material/Grid'
 import Typography from '@mui/material/Typography'
 
+// ** Hooks
+import { useState, useEffect } from 'react';
+
 // ** Scheduler
 import Paper from '@mui/material/Paper';
 import { ViewState } from '@devexpress/dx-react-scheduler';
 import {
   Scheduler,
   MonthView,
+  WeekView,
   Toolbar,
   DateNavigator,
   Appointments,
@@ -15,18 +19,30 @@ import {
 } from '@devexpress/dx-react-scheduler-material-ui';
 
 // ** Demo Components Imports
-
+const schedulerData = [
+  { startDate: '2022-11-11T09:30', endDate: '2022-11-11T11:00', title: 'Meeting Ketiga' },
+  { startDate: '2022-11-12T12:00', endDate: '2022-11-12T13:30', title: 'Meeting Keempat' },
+  { startDate: '2022-11-10T15:00', endDate: '2022-11-10T18:00', title: 'Meeting Kedua' },
+  { startDate: '2022-11-09T14:00', endDate: '2022-11-09T16:00', title: 'Meeting Perdana' },
+];
 
 const Meeting = () => {
+  const [currentDate, setCurrentDate] = useState('2022-11-11');
+  const currentDateChange = (currentDate) => { setCurrentDate(currentDate); };
   return (
     <Paper>
         <Scheduler
-          
+          data={schedulerData}
+          height={660}
         >
           <ViewState
-            defaultCurrentDate="2022-11-11"
+            currentDate={currentDate}
+            onCurrentDateChange={currentDateChange}
           />
-          <MonthView />
+          <WeekView
+            startDayHour={9}
+            endDayHour={19}
+          />
           <Toolbar />
           <DateNavigator />
           <TodayButton />
