0f12fa2d19773e20fb3324025d1d404c99d3327c
arfiandys <222011791@stis.ac.id>
2022-11-11 23:36:17 +0700

    menambahkan detail meeting di user ketika diklik kotaknya

--- src/pages/meeting/index.js
+++ src/pages/meeting/index.js
@@ -15,6 +15,7 @@ import {
   Toolbar,
   DateNavigator,
   Appointments,
+  AppointmentTooltip,
   TodayButton,
 } from '@devexpress/dx-react-scheduler-material-ui';
 
@@ -31,27 +32,28 @@ const schedulerData = [
 const Meeting = () => {
   const [currentDate, setCurrentDate] = useState('2022-11-11');
   const currentDateChange = (currentDate) => { setCurrentDate(currentDate); };
-  
+
   return (
     <Paper>
-        <Scheduler
-          data={schedulerData}
-          height={660}
-        >
-          <ViewState
-            currentDate={currentDate}
-            onCurrentDateChange={currentDateChange}
-          />
-          <WeekView
-            startDayHour={9}
-            endDayHour={19}
-          />
-          <Toolbar />
-          <DateNavigator />
-          <TodayButton />
-          <Appointments />
-        </Scheduler>
-      </Paper>
+      <Scheduler
+        data={schedulerData}
+        height={660}
+      >
+        <ViewState
+          currentDate={currentDate}
+          onCurrentDateChange={currentDateChange}
+        />
+        <WeekView
+          startDayHour={9}
+          endDayHour={19}
+        />
+        <Toolbar />
+        <DateNavigator />
+        <TodayButton />
+        <Appointments />
+        <AppointmentTooltip />
+      </Scheduler>
+    </Paper>
   )
 }
 
