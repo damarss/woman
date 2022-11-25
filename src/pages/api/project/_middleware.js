ef00611f6175b7a215ce57fd6021ff14f9d17dc4
Damars <damarsepti627@gmail.com>
2022-11-25 14:10:33 +0700

    Fetch project, halaman login/register, coba middleware di api project

---
+++ src/pages/api/project/_middleware.js
@@ -0,0 +1,6 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+export async function middleware(req) {
+    // const token = await AsyncStorage.getItem("jwt")
+    // console.log(token)
+}
